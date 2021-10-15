7c0a6a709421614109d8017b6c78a5c3f35d638c
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-10-15 11:08:07 +0300

    header
    js

--- assets/js/header-burger.js
+++ assets/js/header-burger.js
@@ -1,5 +1,5 @@
 $(function () {
-  $('.header-burger').click(function (event) {
+  $('.header__mob .header-burger').click(function (event) {
     $('.header-burger,.header-menu').toggleClass('active')
     $('.body').toggleClass('lock')
   })
@@ -7,27 +7,17 @@ $(function () {
     $('.header-burger,.header-menu').toggleClass('active')
     $('.body').toggleClass('lock')
   })
+  $('.main').click(function (event) {
+    $('.header-burger,.header-menu').removeClass('active')
+  })
 
   $('.menu-list__item .menu-link').on('click', function () {
-    if ($(this).hasClass('show-list')) {
-      $(this).next().slideToggle('slow')
-      $(this).toggleClass('show-list')
-    } else {
-      $('.menu-link').removeClass('show-list')
-      $('.menu-link').next().slideUp()
-      $(this).next().slideToggle('slow')
-      $(this).toggleClass('show-list')
-    }
+    $('.menu-link').removeClass('active')
+    $(this).addClass('active')
+    $('.header-burger,.header-menu').toggleClass('active')
   })
-
-  $('.lang-list  li').map(function (index, el) {
-    if (!$(el).hasClass('current-lang')) {
-      $(this).insertAfter($('.lang-list  li:last'))
-    } else {
-      $(this).find('a').attr('href', 'javascript:void(0)')
-    }
-  })
-  $('.lang-list  .current-lang').click(function () {
-    $('.lang-list  li').toggleClass('active')
+  $('.menu-list__item .menu-link').hover(function () {
+    $('.menu-link').removeClass('active')
+    $(this).toggleClass('active')
   })
 })
