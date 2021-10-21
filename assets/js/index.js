49c238dd5b3c4df59ca0dcaa4da57f349fd02c8c
SergeyVishnevskiy <serrzik13manager@gmail.com>
2021-10-21 10:56:14 +0300

    index
    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,10 @@
 let vh = window.innerHeight * 0.01
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+$(document).ready(function () {
+  $('#faq-accordion').accordionjs({
+    closeAble: true,
+    slideSpeed: 500,
+  })
+})
